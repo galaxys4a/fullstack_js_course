acce3ed483a41dfb39e583f471250f6c3b157898
galaxys4a <63400017+galaxys4a@users.noreply.github.com>
2021-06-24 22:09:31 +0300

    Update index.js

--- webpack-config/src/index.js
+++ webpack-config/src/index.js
@@ -3,6 +3,6 @@
 //document.body.append(page);
 
 import ReactDOM from 'react-dom';
-import Page from './page/index_react.jsx'
+import Page from './page/index_react.js'
 
 ReactDOM.render(Page, document.getElementById('root'));
