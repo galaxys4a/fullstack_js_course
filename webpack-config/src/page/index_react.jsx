2ad0425b7afdbbbe47c5040b8ffd493e71d08dfa
galaxys4a <63400017+galaxys4a@users.noreply.github.com>
2021-06-24 22:17:23 +0300

    Update index_react.jsx

--- webpack-config/src/page/index_react.jsx
+++ webpack-config/src/page/index_react.jsx
@@ -10,25 +10,21 @@ const H1 = React.createElement(
 );
 */
 
-const H1 = () => {
-    return (
-        <h1 className={style.appHeader}>
-            Hello world!
-        </h1>
-    )
-};
+const H1 = () => (
+    <h1 className={style.appHeader}>
+        Hello world!
+    </h1>
+);
 
 /*
 const Page = React.createElement('div', null, Header, H1);
 */
 
-const Page = () => {
-    return (
-        <div>
-            <Header></Header>
-            <H1></H1>
-        </div>
-    )
-};
+const Page = () => (
+    <div>
+        <Header></Header>
+        <H1></H1>
+    </div>
+);
 
 export default Page;
